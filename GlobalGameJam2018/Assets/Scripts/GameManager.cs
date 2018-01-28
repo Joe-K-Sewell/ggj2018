b723275e2e41e08e083cecfe5152b80ca773a53f
Alex DeCamillo <itintern@rockhall.org>
2018-01-27 19:48:25 -0500

    Message mini-game works.. I think

---
+++ GlobalGameJam2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance = null;
+
+	int numCell = 0;
+	int numLaptop = 0;
+	int numTablet = 0;
+
+	// Use this for initialization
+	void Awake () {
+		// ensure only one GameManager exists at a time
+		if (Instance == null)
+			Instance = this;
+		else if (Instance != this)
+			Destroy (gameObject);
+
+		// make sure the manager isn't destoryed between scenes
+		DontDestroyOnLoad (gameObject);
+	}
+
+}
--- GlobalGameJam2018/Assets/Scripts/MessageGame.cs
+++ GlobalGameJam2018/Assets/Scripts/MessageGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MessageGame : MonoBehaviour {
 
@@ -10,61 +11,72 @@ public class MessageGame : MonoBehaviour {
 	public GameObject messageContainer;
 	public GameObject inputContainer;
 	public GameObject text;
+	public GameObject panel;
+	public GameObject prompt;
 
+	public int msgLength = 10;
+
+	[HideInInspector]
 	public bool inputLocked;
 
 	string message;
-	int msgLength = 10;
+
 	int currentMsgPosition = 0;
 	int currentInputPosition = 0;
 
 	float msgInterval = 0.4f;
-	public Text display;
 
 	// Use this for initialization
 	void Start () {
-		inputLocked = false;
+		inputLocked = true;
 		Instance = this;
 		GenerateMessage ();
 		FillContainers ();
 	}
 
+	// fills the containers with empty text components
 	void FillContainers () {
 		for (int i = 0; i < msgLength; i++) {
 			GameObject msgBox = (GameObject)Instantiate (text);
-			msgBox.GetComponent<Text> ().text = "";
+			msgBox.GetComponentInChildren<Text> ().text = "";
 			msgBox.transform.SetParent (messageContainer.transform);
 
 			GameObject inputBox = (GameObject)Instantiate (text);
-			inputBox.GetComponent<Text> ().text = "";
+			inputBox.GetComponentInChildren<Text> ().text = "";
 			inputBox.transform.SetParent (inputContainer.transform);
 		}
 	}
 
 	// displays the message up to the passed msgPosition
 	public IEnumerator DisplayMessage (int msgPosition) {
+		SetPrompt ("Remember the message!");
+		inputLocked = true;
+
 		StartCoroutine (EraseMessage());
-		for (int i = 0; i < msgPosition; i++) {
+		for (int i = 0; i <= msgPosition; i++) {
 			yield return new WaitForSeconds (msgInterval);
-			messageContainer.transform.GetChild (i).GetComponent<Text>().text = message[i].ToString();
+			messageContainer.transform.GetChild (i).GetComponentInChildren<Text>().text = message[i].ToString();
 		}
 	}
 
+	// erases all the text components in the message boxes
 	IEnumerator EraseMessage() {
 		yield return new WaitForSeconds (0.5f + (msgLength*.08f));
-		for (int i = 0; i < messageContainer.transform.childCount; i++) {
+		for (int i = 0; i <= currentMsgPosition; i++) {
 			yield return new WaitForSeconds (msgInterval);
-			messageContainer.transform.GetChild (i).GetComponent<Text> ().text = "";
+			messageContainer.transform.GetChild (i).GetComponentInChildren<Text> ().text = "";
 		}
+
+		SetPrompt ("Enter the message!");
+		inputLocked = false;
 	}
 
+	// places the passed string into the next open box
 	public void RecieveInput (string input) {
-		Debug.Log (currentInputPosition);
-		Debug.Log ("Input is " + input);
-		inputContainer.transform.GetChild (currentInputPosition).GetComponent<Text>(). text = input;
+		inputContainer.transform.GetChild (currentInputPosition).GetComponentInChildren<Text>(). text = input;
 		if (currentInputPosition == currentMsgPosition) {
 			CheckInput ();
-			ClearInput ();
+			StartCoroutine(ClearInput ());
 			currentInputPosition = 0;
 			currentMsgPosition++;
 		} else {
@@ -72,23 +84,43 @@ public class MessageGame : MonoBehaviour {
 		}
 	}
 
-	void ClearInput () {
+	// clears all the input strings
+	IEnumerator ClearInput () {
+		yield return new WaitForSeconds (1);
 		for (int i = 0; i < inputContainer.transform.childCount; i++) {
-			inputContainer.transform.GetChild (i).GetComponent<Text>().text = "";
+			inputContainer.transform.GetChild (i).GetComponentInChildren<Text>().text = "";
+			inputContainer.transform.GetChild (i).GetComponent<Image>().color = new Color(0,0,0,0);
 		}
+		yield return new WaitForSeconds(2);
+		StartCoroutine(DisplayMessage (currentMsgPosition));
+		yield break;
+	}
+
+	// prompts the users to get ready for the message and to recall the message
+	void SetPrompt (string message) {
+		prompt.GetComponent<TextMeshProUGUI> ().text = message;
+	}
+
+	public void onStart () {
+		panel.SetActive (false);
+		StartCoroutine (StartGame());
+	}
+
+	IEnumerator StartGame () {
+		SetPrompt ("Get Ready!!");
+		yield return new WaitForSeconds (2);
+		StartCoroutine(DisplayMessage (currentMsgPosition));
 	}
 
 	// checks the users input to make sure it matches
 	void CheckInput () {
-		for (int i = 0; i < currentMsgPosition; i++) {
-			if (inputContainer.transform.GetChild(i).GetComponent<Text>().text == message[i].ToString() ) {
-				// correct
-				Debug.Log ("correct");
-				// mark box green
+		for (int i = 0; i <= currentMsgPosition; i++) {
+			if (inputContainer.transform.GetChild(i).GetComponentInChildren<Text>().text == message[i].ToString() ) {
+				// correct, mark box green
+				inputContainer.transform.GetChild (i).GetComponent<Image>().color = Color.green;
 			} else {
-				//false
-				Debug.Log ("incorrect");
-				// mark box red
+				// false, mark box red
+				inputContainer.transform.GetChild (i).GetComponent<Image>().color = Color.red;
 			}
 		}
 	}
