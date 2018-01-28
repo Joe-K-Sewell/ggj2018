7b041dbd5acb1e98b07a102d62b98c6f26043bcc
Joe Sewell <Joe-K-Sewell@users.noreply.github.com>
2018-01-27 20:39:46 -0500

    Menu setup...

---
+++ GlobalGameJam2018/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonScript : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ GlobalGameJam2018/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    public string[] Labels;
+    public Vector2[] Locations;
+    public string[] Scenes;
+
+    public GameObject TextObject;
+    private TMPro.TMP_Text _textComponent;
+
+    public GameObject CursorObject;
+    private UnityEngine.UI.Image _cursorComponent;
+    
+    private int _index = 0;
+    
+	// Use this for initialization
+	void Start () {
+        _textComponent = TextObject.GetComponent<TMPro.TMP_Text>();
+        _cursorComponent = CursorObject.GetComponent<UnityEngine.UI.Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _index = (_index - 1) % Labels.Length;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _index = (_index + 1) % Labels.Length;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(Scenes[_index]);
+            return;
+        }
+        else
+        {
+            return;
+        }
+
+        _textComponent.text = Labels[_index];
+        _cursorComponent.transform.position = Locations[_index];
+	}
+}
