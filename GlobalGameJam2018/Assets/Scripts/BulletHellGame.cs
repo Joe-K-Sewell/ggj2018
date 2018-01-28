096107ce570329924178743fe9e37fdf228d4d2f
Kyle Conel <kyleconel@yahoo.com>
2018-01-27 20:10:44 -0500

    Added Descriptions to Other minigames

---
+++ GlobalGameJam2018/Assets/Scripts/BulletHellGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHellGame : MonoBehaviour {
+
+    public GameObject panel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Time.timeScale = 0;	
+	}
+	
+	// Update is called once per frame
+	
+    public void onStart()
+    {
+        panel.SetActive(false);
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        Time.timeScale = 1;
+    }
+}
