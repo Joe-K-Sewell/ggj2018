61693fde8c177756f372b6b7efdf22914df484fb
Alex DeCamillo <itintern@rockhall.org>
2018-01-28 14:19:22 -0500

    Credit scenes finished

---
+++ GlobalGameJam2018/Assets/Scripts/Credits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+	public void onTitle () {
+		SceneManager.LoadScene("Title", LoadSceneMode.Single);
+	}
+
+	public void onQuit () {
+		Application.Quit ();
+	}
+
+}
